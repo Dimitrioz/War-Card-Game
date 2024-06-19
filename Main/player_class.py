6528c0c0673903808dde46d9bf6cfb4ec91d42f2
ΔΗΜΗΤΡΙΟΣ ΓΚΟΡΟΣ <100197395+Dimitrioz@users.noreply.github.com>
2024-06-19 18:21:47 +0300

    Mini Change

--- Main/player_class.py
+++ Main/player_class.py
@@ -8,7 +8,7 @@ class Player:
         return self.all_cards.pop(0)
     
     def add_cards(self, new_cards):
-        if isinstance(new_cards, list):
+        if type(new_cards) == type([]):
             # List of multiple Card objects
             self.all_cards.extend(new_cards)
         else:
